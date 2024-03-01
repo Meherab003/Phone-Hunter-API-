66e62b65f33142cd473d1741fd22984c65fc16d8
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-03-02 00:48:32 +0600

    Loading spinner add

--- js/phone.js
+++ js/phone.js
@@ -9,9 +9,20 @@ const loadPhone = async (searchText) => {
 const displayPhones = phones => {
     const phoneContainer = document.getElementById("phone-container") 
 
+    //show More button when there are more the 12 results
+    const showMoreContainer = document.getElementById("show-more-container");
+
+    if(phones.length > 12){
+        showMoreContainer.classList.remove('hidden');
+    }else{
+        showMoreContainer.classList.add('hidden');
+    }
+
     //clear previous search with the new search;
     phoneContainer.textContent = '';
 
+    //Show upto 12 result maximum
+    phones = phones.slice(0,12)
 
     // console.log(phones);
     phones.forEach(phone =>{
@@ -20,7 +31,7 @@ const displayPhones = phones => {
         phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
         phoneCard.innerHTML = `
         <div class="mx-auto">
-            <figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <figure class="mt-4"><img src="${phone.image}" alt="Shoes" /></figure>
              <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                  <p>Model: ${phone.slug}</p>
@@ -32,14 +43,26 @@ const displayPhones = phones => {
         `
 
         phoneContainer.appendChild(phoneCard)
+
+        toggleLoadingInfinity(false);
     })
 }
 
 const handleSearch = () => {
+    toggleLoadingInfinity(true)
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
     console.log(searchText);
     loadPhone(searchText);
+}
+
+const toggleLoadingInfinity = (isLoading) => {
+    const loadingInfinity = document.getElementById("loading");
+    if(isLoading){
+        loadingInfinity.classList.remove('hidden')
+    }else{
+        loadingInfinity.classList.add("hidden")
+    }
 }
 
 // loadPhone();
